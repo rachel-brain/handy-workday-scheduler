74d208de492684209e220b22751891a7fcea2428
Shilo <rachel.brain@internode.on.net>
2021-06-22 22:12:41 +1000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var displayEl = document.querySelector(".display-3");
+var leadEl = document.querySelector(".lead");
+var currentDayEl = document.querySelector("#currentDay");
+var containerEl = document.querySelector(".container");
+
+var saveBtn = document.querySelector(".save-button");
